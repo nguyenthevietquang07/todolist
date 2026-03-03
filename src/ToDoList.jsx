65d6a34515fc5435036f2cd019c6874b9f167910
nguyenthevietquang <nguyenthevietquang@berkeley.edu>
2026-03-02 19:49:00 -0800

    Clean up repo structure: remove duplicate files and folders

--- src/ToDoList.jsx
+++
@@ -1,8 +0,0 @@
-export default function ToDoList({ id, task, completed, onToggle }) {
-  return (
-    <div className="card">
-      <input type="checkbox" checked={completed} onChange={()=>onToggle(id)}/>
-      <span className={completed ? "completed" : ""}> {task} </span>
-      </div>
-  );
-}
--- src/data.jsx
+++
@@ -1,6 +0,0 @@
-
-export const initialTasks = [
-  { id: 1, task: "Study", completed: false },
-  { id: 2, task: "Workout", completed: true }
-]
-
